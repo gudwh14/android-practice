ee20e42a8dbeb50b7c4f33ac6e41b4fee931821c
gudwh14 <alxious@naver.com>
2021-09-01 14:21:55 +0900

    feat : 홈화면에 User 정보 추가

---
+++ TodoApp/app/src/main/java/jjo/practice/todo/data/model/UserEntity.kt
@@ -0,0 +1,3 @@
+package jjo.practice.todo.data.model
+
+data class UserEntity(var userName : String)
--- TodoApp/app/src/main/java/jjo/practice/todo/presentation/home/HomeFragment.kt
+++ TodoApp/app/src/main/java/jjo/practice/todo/presentation/home/HomeFragment.kt
@@ -6,13 +6,31 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import jjo.practice.todo.R
+import jjo.practice.todo.data.model.UserEntity
+import jjo.practice.todo.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
+    // ViewBinding 이용하기
+    private var _binding : FragmentHomeBinding? = null
+    private val binding get() = _binding!!
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        _binding = FragmentHomeBinding.inflate(inflater, container,false)
+        val view = binding.root
+        return view;
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        onFetchUser()
+    }
+
+    private fun onFetchUser() {
+        val user : UserEntity = UserEntity("JJo")
+        //ViewBinding 을 이용해서 UI Update
+        binding.tvHomeUser.text = user.userName
     }
 }
